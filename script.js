a578389aeb443da61e05fd65399081f51af10b09
lane-andreas <134257045+lane-andreas@users.noreply.github.com>
2023-07-31 20:22:32 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function fewpintsCopy() {
+  // Get the text field
+  var copyText = document.getElementById("fewpints");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
+
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
